71ec480ab25b425de6ed15f765c040f6f23e8140
Artur Bomtempo Colen <arturbcolen@gmail.com>
2025-07-10 11:34:31 -0300

    feat: initialize project with Express server and contact form

---
+++ hands-on/02-step-1/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+let ultimoContato = null;
+
+app.get('/contato', (res, req) => {
+    res.sendFile(path.join(__dirname, '/contato.html'))
+});
+
+app.get('/not-found', (req, res) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
+app.post('/contato', (req, res) => {
+    ultimoContato = req.body;
+    res.redirect('/contato-recebido');
+});
+
+app.get('/contato-recebido', (req, res) => {
+    if (ultimoContato === null) {
+        return res.redirect('/');
+    }
+
+    const { name, email } = ultimoContato;
+
+    res.send(`
+            <h1>Contato recebido! Obrigado, ${name}</h1>
+            <p><strong>E-mail:</strong> ${email}</p>
+        `);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
